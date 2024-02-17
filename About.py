2cd9ecfef86575d6a60ff7704373c73c6d954026
Sonibadu17 <136171636+Sonibadu17@users.noreply.github.com>
2024-02-17 23:29:19 +0530

    Add files via upload

---
+++ About.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Soni's Portfolio",
+    page_icon=":wave:",
+)
+with st.sidebar:
+    st.write("Data Engineer at Infosys Ltd.")
+st.write("# Welcome to Soni's Job Profile! :wave:")
+st.markdown("""
+     I'm **Soni Badu** having 3.6 years of experienced with a strong background in **Unix, API integration, data pipelines using Python, SQL**, and a passion for working with data. 
+
+     Skilled in **designing, implementing, and maintaining large volumes of data.** Proficient in leveraging APIs to extract data from diverse sources and integrate it into efficient data pipelines. 
+
+     Experienced in **SQL for querying and manipulating relational databases**. Adept at optimizing data workflows for speed, scalability, and reliability. 
+
+     Committed to delivering high-quality data solutions that drive actionable insights and business value.
+
+""")
