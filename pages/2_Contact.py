2ddf9038887222610713462561d5a4cf9007ffb3
Sonibadu17 <136171636+Sonibadu17@users.noreply.github.com>
2024-02-17 23:39:56 +0530

    Update 2_Contact.py

--- pages/2_Contact.py
+++ pages/2_Contact.py
@@ -1,7 +1,8 @@
 import streamlit as st
+
+st.set_page_config(page_title="Contact Page")
 with st.sidebar:
     st.write("Data Engineer at Infosys Ltd.")
-st.set_page_config(page_title="Contact Page")
 st.title("**Get in touch :lower_left_fountain_pen:**")
 st.write("**Email :e-mail:** : sonibadu153@gmail.com")
 st.write("**LinkedIn Profile** : https://www.linkedin.com/in/soni-badu/")
