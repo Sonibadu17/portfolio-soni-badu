92c2af836fa346f7d862fadcc13e3e9fe3520020
Sonibadu17 <136171636+Sonibadu17@users.noreply.github.com>
2024-02-17 23:32:18 +0530

    Add files via upload

---
+++ pages/2_Contact.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Contact Page")
+st.title("**Get in touch :lower_left_fountain_pen:**")
+st.write("**Email :e-mail:** : sonibadu153@gmail.com")
+st.write("**LinkedIn Profile** : https://www.linkedin.com/in/soni-badu/")
+st.write("**GitHub Profile** : https://github.com/Sonibadu17")
