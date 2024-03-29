b87e548a58a067ece1132f5a746451cb2086db59
Sonibadu17 <136171636+Sonibadu17@users.noreply.github.com>
2024-03-29 22:49:27 +0530

    Update About.py
    New Features addition -1

--- About.py
+++ About.py
@@ -6,6 +6,10 @@ st.set_page_config(
 )
 with st.sidebar:
     st.write("Data Engineer at Infosys Ltd.")
+    st.write("**Upcoming** Senior Data Analyst at Accenture")
+    file_path = 'Soni_Badu_Resume.pdf'
+    st.markdown(f'<a href="{file_path}">Resume</a>', unsafe_allow_html=True)
+    
 st.write("# Welcome to Soni's Job Profile! :wave:")
 st.markdown("""
      I'm **Soni Badu** having 3.6 years of experienced with a strong background in **Unix, API integration, data pipelines using Python, SQL**, and a passion for working with data. 
