0b228734198d312bf462ca82ec5f9e15d11ee540
Sonibadu17 <136171636+Sonibadu17@users.noreply.github.com>
2024-03-29 22:53:10 +0530

    Adding Resume part

--- pages/2_Contact.py
+++ pages/2_Contact.py
@@ -3,6 +3,10 @@ import streamlit as st
 st.set_page_config(page_title="Contact Page")
 with st.sidebar:
     st.write("Data Engineer at Infosys Ltd.")
+    st.write("**Upcoming** Senior Data Analyst at Accenture")
+    file_path = 'Soni_Badu_Resume.pdf'
+    st.markdown(f'<a href="{file_path}">Resume</a>', unsafe_allow_html=True)
+    
 st.title("**Get in touch :lower_left_fountain_pen:**")
 st.write("**Email :e-mail:** : sonibadu153@gmail.com")
 st.write("**LinkedIn Profile** : https://www.linkedin.com/in/soni-badu/")
