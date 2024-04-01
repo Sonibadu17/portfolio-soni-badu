5ee85a12baad823be18a97e2f9d1dad932fb31f5
Sonibadu17 <136171636+Sonibadu17@users.noreply.github.com>
2024-04-01 16:25:01 +0530

    Update 2_Contact.py

--- pages/2_Contact.py
+++ pages/2_Contact.py
@@ -4,8 +4,18 @@ st.set_page_config(page_title="Contact Page")
 with st.sidebar:
     st.write("Data Engineer at Infosys Ltd.")
     st.write("**Upcoming** Senior Data Analyst at Accenture")
-    file_path = 'Soni_Badu_Resume.pdf'
-    st.markdown(f'<a href="{file_path}">Resume</a>', unsafe_allow_html=True)
+
+    dir = path.Path(__file__).abspath()
+    sys.path.append(dir.parent.parent)
+
+    file_path = './assets/Soni_Badu_Resume.pdf'
+    file_pathm = os.path.join(f"{file_path}")
+    with open(f"{file_pathm}", "rb") as pdf_file:
+        PDFbyte = pdf_file.read()
+    st.download_button(label="Resume",
+                    data=PDFbyte,
+                    file_name="Soni_Badu_Resume.pdf",
+                    mime='application/octet-stream')
     
 st.title("**Get in touch :lower_left_fountain_pen:**")
 st.write("**Email :e-mail:** : sonibadu153@gmail.com")
